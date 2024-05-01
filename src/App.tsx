4138afd3847b948705b15c2599f089df95a0df74
jeetpatel313 <jp2398d@gmail.com>
2024-04-30 17:07:05 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignUpForm from './SignUpForm'; // Assuming SignUpForm.js or SignUpForm.tsx is in the same directory
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>AAK TELE-SCIENCE API - Sign Up Form</h3>
+        <SignUpForm apiUrl="https://django-dev.aakscience.com/signup/" />
+        <h5>Developed by Jeet Patel</h5>
       </header>
     </div>
   );
