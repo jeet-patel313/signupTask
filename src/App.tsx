3e06b5627b4b96d0722f6f529b5cbe8fddd9c8cf
jeetpatel313 <jp2398d@gmail.com>
2024-04-30 17:18:12 -0700

    updated main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './App.css';
-import SignUpForm from './SignUpForm'; // Assuming SignUpForm.js or SignUpForm.tsx is in the same directory
+import SignUpForm from './components/SignUpForm'; // Assuming SignUpForm.js or SignUpForm.tsx is in the same directory
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h3>AAK TELE-SCIENCE API - Sign Up Form</h3>
-        <SignUpForm apiUrl="https://django-dev.aakscience.com/signup/" />
-        <h5>Developed by Jeet Patel</h5>
       </header>
+      <main>
+        <SignUpForm apiUrl="https://django-dev.aakscience.com/signup/" />
+      </main>
+      <footer>
+        <h5>Developed by Jeet Patel</h5>
+      </footer>
     </div>
   );
 }
